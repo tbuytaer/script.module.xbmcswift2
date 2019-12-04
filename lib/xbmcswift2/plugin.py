e47b73c0805a8b0d9947b8153a5342ad5c9d753f
enen92 <92enen@gmail.com>
2019-12-04 10:30:00 +0000

    New language paths with fallback

--- lib/xbmcswift2/plugin.py
+++ lib/xbmcswift2/plugin.py
@@ -125,6 +125,9 @@ class Plugin(XBMCMixin):
             else:
                 addon_dir = os.getcwd()
             strings_fn = os.path.join(addon_dir, 'resources', 'language',
+                                      'resource.language.en_gb', 'strings.po')
+            if not os.path.exists(strings_fn):
+                strings_fn = os.path.join(addon_dir, 'resources', 'language',
                                       'English', 'strings.po')
             utils.load_addon_strings(self._addon, strings_fn)
 
